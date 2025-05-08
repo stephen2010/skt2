1ce67475cc681c50fc2b25378808f22085f9aa71
stephen2010 <59519865+stephen2010@users.noreply.github.com>
2025-05-08 14:34:32 +0800

    Update db connect

--- utils/db.ts
+++ utils/db.ts
@@ -1,6 +1,7 @@
 /// <reference lib="deno.unstable" />
 
-const kv = await Deno.openKv();
+//const kv = await Deno.openKv();
+const kv = await Deno.openKv("https://api.deno.com/databases/8fe05f0b-e7af-4619-bcf5-05f599a4bd13/connect");
 
 export interface Item {
   title: string;
