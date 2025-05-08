2db6a9316e6f9debefd01eb94a63277a83cabbe3
stephen2010 <59519865+stephen2010@users.noreply.github.com>
2025-05-08 15:39:59 +0800

    Update db.ts

--- utils/db.ts
+++ utils/db.ts
@@ -1,4 +1,3 @@
-/*
 const kv = await Deno.openKv();
 
 export interface Item {
@@ -6,18 +5,20 @@ export interface Item {
   url: string;
 }
 
-export async function createItem(item: Item) {
-  const itemsKey = ["items","test1"];
+export async function createItem(title: String, url: String) {
+  const itemsKey = ["items", "test1"];
 
   const res = await kv.atomic()
-    .set(itemsKey, item)
+    .set(itemsKey, {
+      title,
+      url,
+    })
     .commit();
 
   if (!res.ok) throw new Error("Failed to create item");
 }
 
 export async function getItem() {
-  const res = await kv.get<Item>(["items","test1"]);
+  const res = await kv.get<Item>(["items", "test1"]);
   return res.value;
 }
-*/
